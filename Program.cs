43bd868aaf65e6e78d975244f037d8317f439dd1
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-07-22 20:56:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static string GenerateText()
+    {
+        return "Hello from C#!";
+    }
+
+    static void Main()
+    {
+        string text = GenerateText();
+        Console.WriteLine("Generated text: " + text);
+    }
+}
